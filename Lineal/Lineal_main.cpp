fad2010ca05327c04f20da4161c399f41579fcc9
JuanGon142857 <juan.gonzalezm1@udea.edu.co>
2023-05-10 23:25:19 -0500

    documentacion

--- Lineal/Lineal_main.cpp
+++ Lineal/Lineal_main.cpp
@@ -1,9 +1,9 @@
 #include "Lineal.h" 
 
 int main() {
-    Lineal myLineal(0, 50, 0, 0, 49);
-    myLineal.Set_x();
-    myLineal.Solve();
-    myLineal.Solution2csv("Ejercicio 8 libro.csv");
+    Lineal myLineal(0, 50, 0, 0, 49); //x0, xf, y0, yf, N - 1 intervalos
+    myLineal.Set_x();   //Encuentra los puntos x
+    myLineal.Solve();   //Encuentra las aproximasciones y
+    myLineal.Solution2csv("Ejercicio 8 libro.csv"); //El argumento es el nombre del archivo donde se guardaran los resultados
 
 }
--- NoLineal/No_Lineal_main.cpp
+++ NoLineal/No_Lineal_main.cpp
@@ -1,10 +1,10 @@
 #include "No_Lineal.h" 
 
 int main() {
-    No_Lineal myNOLineal(0, 120., 0, 0, 19, 50, 1e-5);
+    No_Lineal myNOLineal(0, 120., 0, 0, 19, 50, 1e-5); //x0, xf, y0, yf, N - 1 intervalos, numero maximo de iteraciones del metodo iterativo, tolerancia para definir si se alcazò la convergencia
     //myNOLineal.Set_N( 19);
-    myNOLineal.Set_x();
-    myNOLineal.Solve();
+    myNOLineal.Set_x(); //Encuentra los puntos x
+    myNOLineal.Solve(); //Encuentra las aproximasciones y
     myNOLineal.Solution2csv("Ejercicio 6 libro.csv");
 
 }
